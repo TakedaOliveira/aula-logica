621da8873f51df8933f8cd8cfe375ade3efc0a9f
TakedaOliveira <eric.8444@colegioprogresso.g12.br>
2026-03-09 12:08:29 -0300

    Criei a função média

---
+++ media.js
@@ -0,0 +1,7 @@
+function media(a, b){
+    return (a + b) / 2
+}
+
+const calculo = media(5, 8)
+
+console.log(calculo)
--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-const numero1 = 8
-const numero2 = 12
-
-function somaNumeros(){
-    console.log("Cheguei aqui")
+function somaNumeros(a, b){
+    return a + b
 }
 
-somaNumeros()
+const soma1 = somaNumeros(8, 15)
+const soma2 = somaNumeros(15, 17)
+const soma3 = somaNumeros(7, 16)
+const soma4 = somaNumeros(81, 105)
+
+console.log(soma1)
+console.log(soma2)
+console.log(soma3)
+console.log(soma4)
